8ab93560424e4a7a6bb43c82a5f1c8ce782f6dbf
yassine mansour <yassineaitman@gmail.com>
2026-03-11 09:09:34 +0000

    make the supplier controller extends BaseController and override their methods

--- app/Controllers/SupplierController.php
+++ app/Controllers/SupplierController.php
@@ -6,10 +6,34 @@ namespace App\Controllers;
 
 use App\View;
 
-class SupplierController
+class SupplierController extends BaseController
 {
     public function index(): View
     {
         return View::make('suppliers/index');
+    }
+
+    public function add(): View {
+        return View::make('');
+    }
+
+    public function store() {
+
+    }
+
+    public function showInfo(): View {
+        return View::make('');
+    }
+
+    public function remove() {
+
+    }
+
+    public function edit(): View {
+        return View::make('');
+    }
+
+    public function update() {
+
     }
 }
